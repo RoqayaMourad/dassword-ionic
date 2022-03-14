fc177a4513346c3e4ecbd4a54c346de8db1c9d30
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-03-14 17:02:24 +0200

    main Db model

---
+++ src/app/models/maindb.class.ts
@@ -0,0 +1,56 @@
+import { HelperService } from 'src/app/services/util/helper';
+import { Folder } from './folder.class';
+import { Item } from 'src/app/models/item.class';
+import { IMainDB } from "./interfaces";
+
+
+export class MainDB implements IMainDB {
+
+  constructor() { }
+
+  userId?: string = HelperService.makeid(); // current user's id
+  email?: string = ""; // current user's login email
+  secureObject?: any; // login object
+  objectVersionId?: string;
+  items?: Item[];
+  folders?: Folder[];
+
+  setUserId(userId: string) {
+    this.userId = userId;
+  }
+
+  setEmail(email: string) {
+    this.email = email;
+  }
+
+  setSecureObject(secureObject) {
+    this.secureObject = secureObject
+  }
+
+  setItems(items: Item[]) {
+    this.items = items
+  }
+
+  // Items functions
+  addItem(item: Item) {
+    this.items.push(item)
+  }
+
+  getItem(itemId: string) {
+    return this.items.find((i) => i.itemId == itemId)
+  }
+
+  deleteItem(itemId: string) {
+    this.items.splice(this.items.findIndex(function (i) {
+      return i.itemId == itemId;
+    }), 1);
+  }
+
+  updateItem(item: Item){
+    var item = this.items.find((i) => i.itemId == item.itemId);
+    item.update(item)
+  }
+
+
+
+}
