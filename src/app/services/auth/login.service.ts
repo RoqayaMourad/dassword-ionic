38a71d6ecdf67ec61eab1b73a6bffc84918b90dd
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-03-14 08:28:36 +0200

    basic auth service

---
+++ src/app/services/auth/login.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Api } from "../api/api";
+import { DataService } from "../data/data.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  constructor(private api: Api, private d: DataService) {
+  }
+
+  /**
+   * Register account
+   * when user registers an account he sends his email and an encrypted password to the server to register a new account
+   */
+
+  register(secure_object) {
+    this.api.post('create-db-user', secure_object).subscribe(r => {
+      console.log(r);
+      this.d.alert("Account created:"+JSON.stringify(r,null,1))
+    })
+  }
+
+}
