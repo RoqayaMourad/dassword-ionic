4727c8b2bef52c27eb2ea72b3634b98cb8fec04f
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-03-14 15:03:11 +0200

    new item component

--- src/app/components/_dashboard/_item_details/new-item/new-item.component.ts
+++ src/app/components/_dashboard/_item_details/new-item/new-item.component.ts
@@ -1,6 +1,9 @@
+import { DataService } from './../../../../services/data/data.service';
+import { IItem, ItemType } from './../../../../models/interfaces';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
+import { Item } from 'src/app/models/item.class';
 
 @Component({
   selector: 'app-new-item',
@@ -9,7 +12,7 @@ import { ModalController } from '@ionic/angular';
 })
 export class NewItemComponent implements OnInit {
 
-  constructor(fb: FormBuilder, private modalController: ModalController) {
+  constructor(fb: FormBuilder, private modalController: ModalController, private data:DataService) {
     this.DetailsForm = fb.group({
       name: [''],
       email: [''],
@@ -18,18 +21,71 @@ export class NewItemComponent implements OnInit {
       description: [''],
       notes: [''],
       file: [''],
-      type:['']
+      type: ['']
     });
   }
 
   DetailsForm: FormGroup
 
+
   ngOnInit() {
-    setInterval(() => { console.log(this.DetailsForm) }, 5000)
   }
 
-  async onSubmit(){
+  async onSubmit() {
 
+    // Create item object
+    let item = new Item();
+    this.data.PassDb$
+    // Add item object to the current DB
+
+    // Update current DB
+
+    await this.dismiss();
+  }
+
+  async dismiss(){
     await this.modalController.dismiss();
+  }
+
+
+  //#region  Type options menu handlers
+  typeSelectOpen = false;
+  type: ItemType = "Password";
+
+  setType(t) {
+    console.log("setType(t) t=", t);
+    this.type = t;
+    this.closeSelect()
+  }
+
+  toggleSelect() {
+    this.typeSelectOpen = !this.typeSelectOpen
+  }
+
+  closeSelect() {
+    this.typeSelectOpen = false;
+  }
+  //#endregion
+
+  get showName(){
+    return true
+  }
+  get showEmail(){
+    return this.type == 'Password'
+  }
+  get showPassword(){
+    return this.type == 'Password'
+  }
+  get showUrl(){
+    return this.type == 'Password'
+  }
+  get showDescription(){
+    return this.type == 'Password' || this.type == 'Bank Account' || this.type == 'Card' || this.type == 'Document'
+  }
+  get showNote(){
+    return this.type == 'Note'
+  }
+  get showDocument(){
+    return this.type == 'Document'
   }
 }
